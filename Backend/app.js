3cafd2eedd28bf0edd414facc050cd02f2c96cdc
IdioticMarlin22 <jcrantfo@comcast.net>
2022-06-05 21:47:27 -0400

    Initiated Server and Frontend

--- Backend/app.js
+++ Backend/app.js
@@ -1,36 +1,8 @@
-
-// Imports
+// Import Dependencies
 const express = require('express');
-const path = require('path');
-// const bodyParser = require('body-parser');
-
 
 const app = express();
-app.use(bodyParser.json());
 
-// Routes
-const indexRouter = require('./routes/indexRouter');
-const teamRouter = require('./routes/teamRouter');
-const calcRouter = require('./routes/calculatorRouter');
-app.use('/', indexRouter);
-app.use('/team', teamRouter);
-app.use('/calculator', calcRouter);
-
-
-// Static Files
-app.use(express.static('public'));
-//app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/js', express.static(__dirname + 'public/js'));
-
-
-// Views
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-
-
-// Listen on port 3000
-app.listen(app.get('port'), () => {
-  console.info('App listening on port ' + app.get('port'));
+app.listen(5000, () => {
+  console.log(`App listening on port 5000`)
 });
